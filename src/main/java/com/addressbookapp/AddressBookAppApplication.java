be5cfd05181612bffc4c1af02909ed42a80e0fce
Akash Sharma <i.akashhhh@gmail.com>
2026-03-07 16:19:02 +0530

    [Akash] : Added UC8 feature to search contact by city or state

--- src/main/java/com/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/addressbookapp/AddressBookAppApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AddressBookAppApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		
+		SpringApplication.run(AddressBookAppApplication.class, args);
+		System.out.println("Welcome to Address Book Application");
+		
+		AddressBookMain.start();
+	}
 
-
-        SpringApplication.run(AddressBookAppApplication.class, args);
-        System.out.println("Welcome to Address Book Application");
-
-        AddressBookMain.start();
-    }
 }
